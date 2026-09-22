e7e77f8d57c3890494add19078b8c48c45a7ca0a
Joao Pugliesi <mrlanguages62@gmail.com>
2026-09-22 15:25:23 -0300

    structure/shape notifications and notification components
    structure/shape notifications and notification components

--- js/components/parts/notifications/notifications.js
+++ js/components/parts/notifications/notifications.js
@@ -1,7 +1,15 @@
+import notification from "./parts/notification.js";
+
 function notifications () {
     return `
         <div class="notifications">
-            
+            <div id="notifications-title">
+                <h3>Perfil</h3>
+            </div>
+            <div id="notifications-display">
+                ${notification(1, 'Dai adicionou uma nova observação na transferência XDFGHT', false, '10H')}
+                ${notification(2, 'Dai marcou como concluída uma observação na transferência XYZDFG', true, '10H')}
+            </div>
         </div>
     `
 }
---
+++ js/components/parts/notifications/parts/notification.js
@@ -0,0 +1,30 @@
+function notification (id, description, is_viewed, timestamp) {
+    return `
+    <div class="notification ${is_viewed ? 'viewed' : 'pendent'}">
+        <div class="notification-description">
+            <p>${description}</p>
+        </div>
+        <div class="notification-info">
+            <div class="notification-info-isviewed">
+                <h3>${is_viewed ? 'LIDA' : 'NÃO LIDA'}</h3>
+            </div>
+            <div>
+                <h3 class="notification-info-timestamp" data-timestamp="${timestamp}">Há ${timestamp}</h3>
+            </div>
+        </div>
+        <div class="notification-options">
+            <div class="notification-options-view" data-id="${id}">
+                <i class="fa-solid fa-panorama"></i>
+                <h3>Ver</h3>
+            </div>
+            <div class="notification-options-delete" data-id="${id}">
+                <i class="fa-solid fa-eraser"></i>
+                <h3>Excluir</>
+            </div>
+        </div>
+    </div>
+    
+    `
+}
+
+export default notification;
