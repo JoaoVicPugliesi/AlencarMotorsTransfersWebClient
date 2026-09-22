04bbd42c5cd4c365aca9d560d5b6f71af70e394a
Joao Pugliesi <mrlanguages62@gmail.com>
2026-09-22 18:45:26 -0300

    abstract transfers component
    abstract transfers component

---
+++ js/components/parts/transfers/parts/transfer.js
@@ -0,0 +1,26 @@
+function transfer(status, name, plate, vehicle, code) {
+    return `
+    <div class="transfer ${status}">
+        <div class="transfer-info">
+            <div class="transfer-info-name">
+                <i class="fa-regular fa-id-card"></i>
+                <h3>${name}</h3>
+            </div>
+            <div class="transfer-info-plate">
+                <i class="fa-solid fa-certificate"></i>
+                <h3>${plate}</h3>
+            </div>
+            <div class="transfer-info-vehicle">
+                <i class="fa-solid fa-car"></i>
+                <h3>${vehicle}</h3>
+            </div>
+            <div class="transfer-info-code">
+                <i class="fa-solid fa-code"></i>
+                <h3>${code}</h3>
+            </div>
+        </div>
+    </div>
+    `
+}
+
+export default transfer;
---
+++ js/components/parts/transfers/parts/transfers_searchbar_filters.js
@@ -0,0 +1,12 @@
+function transfers_searchbar_filters() {
+    return `
+    <div class="transfers-searchbar-filters">
+        <input placeholder="Placa">
+        <input placeholder="Veículo">
+        <input placeholder="Código">
+        <input placeholder="Status">
+    </div>
+    `
+}
+
+export default transfers_searchbar_filters;
