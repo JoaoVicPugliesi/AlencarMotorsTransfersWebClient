6579a03dcc0bf5210a37cd3878b94d29a7fffead
Joao Pugliesi <mrlanguages62@gmail.com>
2026-09-22 17:10:32 -0300

    define main component basic structure
    define main component basic structure

--- js/components/page.js
+++ js/components/page.js
@@ -1,14 +1,16 @@
 import header from "./parts/header/header.js";
 import profile from "./parts/profile/profile.js";
 import notifications from "./parts/notifications/notifications.js"
-import login from "./parts/login/login.js";
+import main from "./parts/main/main.js";
+// import login from "./parts/login/login.js";
+// ${login()};
 
 function page () {
     return `
         <div id="page">
             ${header()}
-            ${login()}
             ${profile()}
+            ${main()}
             ${notifications()}
         </div>
     `
---
+++ js/components/parts/main/main.js
@@ -0,0 +1,9 @@
+function main () {
+    return `
+        <div class="main">
+            <h3>Main</h3>
+        </div>
+    `
+}
+
+export default main;
